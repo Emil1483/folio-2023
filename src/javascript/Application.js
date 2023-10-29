e49b35d4c363616dc3c9370a6fa00d978cd313fa
Emil Djupvik <emil14833@gmail.com>
2023-10-29 18:25:23 +0100

    remove threejsJourney

--- src/javascript/Application.js
+++ src/javascript/Application.js
@@ -6,7 +6,6 @@ import Time from './Utils/Time.js'
 import World from './World/index.js'
 import Resources from './Resources.js'
 import Camera from './Camera.js'
-import ThreejsJourney from './ThreejsJourney.js'
 
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
@@ -34,7 +33,6 @@ export default class Application {
         this.setPasses()
         this.setWorld()
         this.setTitle()
-        this.setThreejsJourney()
     }
 
     /**
@@ -240,17 +238,6 @@ export default class Application {
 
             document.title = `${'_'.repeat(this.title.width - this.title.position)}🚗${'_'.repeat(this.title.position)}`
         }, this.title.frequency)
-    }
-
-    /**
-     * Set Three.js Journey
-     */
-    setThreejsJourney() {
-        this.threejsJourney = new ThreejsJourney({
-            config: this.config,
-            time: this.time,
-            world: this.world
-        })
     }
 
     /**
